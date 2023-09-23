deffbf201aba555015e27cfb115c67aa6c3898c7
bru37 <142701350+bru37@users.noreply.github.com>
2023-09-24 02:58:35 +0900

    변경

---
+++ ob.js
@@ -0,0 +1,66 @@
+const Menwebtoons = [
+  {
+    title: "초인의 시대",
+    rank: 1,
+    nickname: "섭이",
+  },
+  {
+    title: "싸움독학",
+    rank: 2,
+    nickname: "박태준 만화회사 / 김정현 스튜디오",
+  },
+  {
+    title: "신혼일기",
+    rank: 3,
+    nickname: "자까",
+  },
+  {
+    title: "입학용병",
+    rank: 4,
+    nickname: "YC / 락현",
+  },
+  {
+    title: "가비지타임",
+    rank: 5,
+    nickname: "2사장",
+  },
+  //맨과우먼을 비교 하려고 만들었습니다 처음에는 우먼앞에 const를 붙혀서 하니 빨간 줄이 있어서 없애니깐 맨과 우면이 비교가 되는듯 합니다
+  ]
+    const womanwebtoons = [
+    {
+      title: "신혼일기",
+      rank: 1,
+      nickname: "자까",
+    },
+    {
+      title: "자매전쟁",
+      rank: 2,
+      nickname: "기맹기",
+    },
+    {
+      title: "가비지타임",
+      rank: 3,
+      nickname: "2사장",
+    },
+    {
+      title: "청춘계시록",
+      rank: 4,
+      nickname: "한서",
+    },
+    {
+      title: "작정명 순정",
+      rank: 5,
+      nickname: "꼬까리 / 들덤",
+    },
+  ]
+
+
+// if ((!Menwebtoons, womanwebtoons.includes("마음에소리"))) {
+// }
+// Menwebtoons, womanwebtoons.push("마음에소리");
+// console.log(Menwebtoons, womanwebtoons);
+// 만약에 맨과우먼웹툰에 마음에소리가 없으면 마음에소리를 넣어라 (추가)
+
+// filter 또는 reduce를 통해 중복된 이름 지우기
+// reverse를 이용해서 순위 뒤집기
+//통합순위만들기
