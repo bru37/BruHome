e954916ec9021395d4c33a16a3d9165bf5003a99
bru37 <142701350+bru37@users.noreply.github.com>
2023-09-25 23:43:20 +0900

    수정본

--- ob.js
+++ ob.js
@@ -72,66 +72,66 @@ const Menwebtoons = [
   ]
 
 // 만약에 맨과우먼웹툰에 마음에소리가 없으면 마음에소리를 넣어라 (추가)
-//  if ((!Menwebtoons, womanwebtoons.includes("마음에소리"))) {
-//  }
-//  Menwebtoons, womanwebtoons.push("마음에소리");
-//  console.log(Menwebtoons, womanwebtoons);
+  if ((!Menwebtoons, womanwebtoons.includes("마음에소리"))) {
+  }
+  Menwebtoons, womanwebtoons.push("마음에소리");
+  console.log(Menwebtoons, womanwebtoons);
 
 
-// if(score=1){
-//    rank="1";
-//  }else if(score=2){
-//   rank="2";
-// }else if(score=3){
-//   rank="3";
-// }else if(score=4){
-//   rank="4";
-//  }else{
-//    score="5";
-//  }
-// console.log(`${Menwebtoons.title}님의 이번 순위는 ${rank}입니다.`);
-// console.log(`${womanwebtoons.title}님의 이번 순위는 ${rank}입니다.`);
+ if(score=1){
+    rank="1";
+  }else if(score=2){
+   rank="2";
+ }else if(score=3){
+   rank="3";
+ }else if(score=4){
+   rank="4";
+  }else{
+    score="5";
+  }
+ console.log(`${Menwebtoons.title}님의 이번 순위는 ${rank}입니다.`);
+ console.log(`${womanwebtoons.title}님의 이번 순위는 ${rank}입니다.`);
 // 순위를 만들어봤느넫 이상하게 나오내요...어렵다
 
 
 
 
 //객체 배열 오름차순
-//  Menwebtoons.sort((a,b)=>{
-//    return a.rank-b.rank
-//  });
-//  console.log(Menwebtoons);
+  Menwebtoons.sort((a,b)=>{
+    return a.rank-b.rank
+  });
+  console.log(Menwebtoons);
 
-//  womanwebtoons.sort((a,b)=>{
-//    return a.rank-b.rank
-//  });
-//  console.log(womanwebtoons);
+  womanwebtoons.sort((a,b)=>{
+    return a.rank-b.rank
+  });
+  console.log(womanwebtoons);
 
 
 //객체 배열 내림차순-오름차순과는 다르게 내림차순 리턴값을 바꾸면된다.
-// Menwebtoons.sort((a,b)=>{
-//   return b.rank-a.rank
-// });
-// console.log(Menwebtoons);
+ Menwebtoons.sort((a,b)=>{
+   return b.rank-a.rank
+ });
+ console.log(Menwebtoons);
 
-// womanwebtoons.sort((a,b)=>{
-//   return b.rank-a.rank
-// });
-// console.log(womanwebtoons);
+ womanwebtoons.sort((a,b)=>{
+   return b.rank-a.rank
+ });
+ console.log(womanwebtoons);
 
 
 
 // filter  중복된 이름 지우기
 
 
-// const result = Menwebtoons.filter((val, idx) => {
-//   return Menwebtoons.indexOf(val) === idx; 
+ const resultA = Menwebtoons.filter((val, idx) => {
+   return Menwebtoons.indexOf(val) === idx; 
 
-// });
+ });
 
-// const result = womanwebtoons.filter((val, idx) => {
-//   return womanwebtoons.indexOf(val) === idx; 
-// });
+ const resultB = womanwebtoons.filter((val, idx) => {
+   return womanwebtoons.indexOf(val) === idx; 
+ });
 
 
 //두개 중복 배열 제거하기...내일 다시해보자 
